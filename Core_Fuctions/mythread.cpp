bda955a0e24e9aba62f7dbc8bd137945db60846f
longlp <longlp@dfm-engineering.com>
2014-10-07 14:22:08 +0700

    changed path of DOFICore

--- Core_Fuctions/mythread.cpp
+++ Core_Fuctions/mythread.cpp
@@ -4,7 +4,8 @@ MyThread::MyThread(QObject *parent) :
     QThread(parent)
 {
     #if defined(Q_OS_WIN)
-        this->sourceOF = "DOFICore\\setvars.bat";
+        this->sourceOF = QApplication::applicationDirPath() + "/DOFICore\\setvars.bat";
+//        this->sourceOF ="C:/Users/Napyidaw/Desktop/DMesh_Working/DMesh-build-desktop/DOFICore\\setvars.bat";
     #else
     QString dofidir = "/home/longlp/Desktop/DOFI_Working/OpenFoamGUI-build-desktop";
     this->sourceOF = "DOFIDIR=" + QApplication::applicationDirPath() + " && source "  + QApplication::applicationDirPath() +  "/DOFICore/openfoam211/etc/bashrc";
